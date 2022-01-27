78e935cd180e99b60fe5536cac55c76ed1d78c61
ShinjiWP <xkounagox@gmail.com>
2022-01-27 22:00:56 +0900

    202201272200

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="<?php language_attributes();?>">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="Shinjiのポートフォリオサイトになります" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>ManStandsUp Portfolio</title>
+  <link href="//fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700;900&display=swap" rel="stylesheet" />
+  <link href="//fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet" />
+  <link rel="icon" type="img.ico" href="<?php echo get_theme_file_uri('/src/image/favicon/favicon.ico');?>" />
+  <link href=" //use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet" />
+  <?php wp_head();?>
+
+</head>
+
+<body>
+  <!-- <div class=" loading">
+  <p class="load-text anim-typewriter c-text--large c-text--weight c-text--underline">Portfolio</p>
+  </div> -->
+  <div id="particles-js-white"></div>
+  <div id="particles-js-green"></div>
+  <div id="particles-js-red"></div>
+  <div id="content"></div>
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php get_header();?>
+
+<div class="c-background--circle"><img class="c-image--circle" src="./src/image/png/circle.png" alt=""></div>
+<div id="wrapper" class="p-wrapper">
+  <div class="c-grid__head p-header">
+    <div class="p-line"></div>
+    <p class="c-text--ttl c-head p-header__ttl"><span>P</span>roducts</p>
+    <p class="c-text--ttl c-head p-header__ttl">Contact</p>
+    <p class="c-text--ttl c-head p-header__ttl">About</p>
+    <p class="c-text--ttl c-head p-header__ttl"><span class="c-dot"></span>Skill</p>
+  </div>
+
+  <div class="p-nav">
+    <a data-hover="Products" class="c-text--weight c-square button js-btn1 p-nav__btn"
+      href="products.html"><span>制</span><span>作</span><span>実</span><span>績</span>
+      <div class="c-hover--btn1"></div>
+    </a>
+    <a data-hover="Contact" class="c-text--weight c-square button js-btn2 p-nav__btn"
+      href="contact.html"><span>問</span><span>い</span><span>合</span><span>わ</span><span>せ</span>
+      <div class="c-hover--btn2"></div>
+    </a>
+    <a data-hover="About" class="c-text--weight c-square button js-btn3 p-nav__btn"
+      href="#"><span>私</span><span>に</span><span>つ</span><span>い</span><span>て</span>
+      <div class="c-hover--btn3"></div>
+    </a>
+    <a data-hover="Skill" class="c-text--weight c-square button js-btn4 p-nav__btn"
+      href="skill.html"><span>ス</span><span>キ</span><span>ル</span>
+      <div class="c-hover--btn4"></div>
+    </a>
+  </div>
+
+  <?php get_template_part('includes/footer');?>
+
+  <?php get_footer();?>
