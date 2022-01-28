4ceb41e6449ecfda52af2c47c80f08e2d7f2f072
ShinjiWP <xkounagox@gmail.com>
2022-01-28 21:11:51 +0900

    202201282110

--- functions.php
+++ functions.php
@@ -3,6 +3,8 @@
 function add_files(){ wp_enqueue_style('style',get_theme_file_uri().'/style.css',array());
 wp_enqueue_style('main-style',get_theme_file_uri().'/src/css/style.css',array());
 wp_enqueue_script('main-script',get_theme_file_uri().'/src/js/assets/3.6.0.min.js',array('jquery'),'1.0.0',true); 
+wp_enqueue_script('particle-script',get_theme_file_uri().'/src/js/assets/particlejs.min.js',array('jquery'),'1.0.0',true); 
+wp_enqueue_script('gsap-script',get_theme_file_uri().'/src/js/assets/gsap.min.js',array('jquery'),'1.0.0',true); 
 wp_enqueue_script('background-script',get_theme_file_uri().'/src/js/assets/background.js',array('jquery'),'1.0.0',true); 
 wp_enqueue_script('dot-script',get_theme_file_uri().'/src/js/assets/dot.js',array('jquery'),'1.0.0',true); 
 wp_enqueue_script('assets-script',get_theme_file_uri().'/src/js/assets/scripts.js',array('jquery'),'1.0.0',true); 
