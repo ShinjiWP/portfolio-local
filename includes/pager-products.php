864096154324e8cfbeec5a3ba141acf4ee4be224
ShinjiWP <xkounagox@gmail.com>
2022-02-06 14:39:54 +0900

    202202061440

--- includes/pager-products.php
+++ includes/pager-products.php
@@ -1,4 +1,7 @@
 <div class="p-nav--link">
+  <div class="c-text--weight c-square c-button--white js-btn2 p-nav__btn">
+    <p>Products</p>
+  </div>
   <a class="c-text--weight c-square c-button--white js-btn2 p-nav__btn"
     href="<?php echo esc_url(get_page_link(54)); ?>">
     <p>Contact</p>
