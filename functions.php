3c4e3634efce161d242305d4974de7790b4a0133
ShinjiWP <xkounagox@gmail.com>
2022-03-10 23:40:32 +0900

    202203102340 同日2回目はローディング画面表示せず、対応

--- functions.php
+++ functions.php
@@ -13,12 +13,12 @@ register_nav_menu( 'side-nav', 'サイドナビゲーション');
 function add_files(){ wp_enqueue_style('style',get_theme_file_uri().'/style.css',array());
 wp_enqueue_style('main-style',get_theme_file_uri().'/src/css/style.css',array());
 wp_enqueue_script('main-script',get_theme_file_uri().'/src/js/assets/3.6.0.min.js',array('jquery'),'1.0.0',true); 
+wp_enqueue_script('cookie-script',get_theme_file_uri().'/src/js/assets/cookie.js',array('jquery'),'1.0.0',true); 
 wp_enqueue_script('particle-script',get_theme_file_uri().'/src/js/assets/particlejs.min.js',array('jquery'),'1.0.0',true); 
 wp_enqueue_script('gsap-script',get_theme_file_uri().'/src/js/assets/gsap.min.js',array('jquery'),'1.0.0',true); 
 wp_enqueue_script('background-script',get_theme_file_uri().'/src/js/assets/background.js',array('jquery'),'1.0.0',true); 
 wp_enqueue_script('dot-script',get_theme_file_uri().'/src/js/assets/dot.js',array('jquery'),'1.0.0',true); 
 wp_enqueue_script('assets-script',get_theme_file_uri().'/src/js/assets/scripts.js',array('jquery'),'1.0.0',true); 
-// wp_enqueue_script('footer-script',get_theme_file_uri().'/javascript/footerFixed.js',array('jquery'),'1.0.0',true); 
 }
 add_action ('wp_enqueue_scripts','add_files');
 
--- src/js/assets/scripts.js
+++ src/js/assets/scripts.js
@@ -50,6 +50,47 @@ jQuery(".js-btn1,.js-btn2,.js-btn3,.js-btn4").on("mouseout", function () {
 });
 
 
+//同じ日付で2回目以降ならローディング画面非表示の設定
+
+var splash_text = $.cookie('accessdate'); //キーが入っていれば年月日を取得
+var myD = new Date(); //日付データを取得
+var myYear = String(myD.getFullYear()); //年
+var myMonth = String(myD.getMonth() + 1); //月
+var myDate = String(myD.getDate()); //日
+
+if (splash_text != myYear + myMonth + myDate) {
+	//cookieデータとアクセスした日付を比較↓
+  $(".p-loading")
+		.delay(10000)
+		.queue(function (next) {
+			$(this).addClass("loaded");
+			$(".p-line").addClass("line-start");
+			$(".p-header__ttl:nth-child(2)").addClass("js-header2");
+			$(".p-header__ttl:nth-child(3)").addClass("js-header3");
+			$(".p-header__ttl:nth-child(4)").addClass("js-header4");
+			$(".p-header__ttl:nth-child(5)").addClass("js-header5");
+			$(".p-front-page__gif").animate({ opacity: 1 }, { duration: 4000, easing: "swing" });
+			$(".p-front-page__circle").animate({ opacity: 0.3 }, { duration: 4000, easing: "swing" });
+      $(".p-nav").animate({ opacity: 1 }, { duration: 3500, easing: "swing" });
+      var myD = new Date();
+					var myYear = String(myD.getFullYear());
+					var myMonth = String(myD.getMonth() + 1);
+					var myDate = String(myD.getDate());
+					$.cookie('accessdate', myYear + myMonth + myDate); //accessdateキーで年月日を記録
+			next();
+		});
+} else {
+	$(".p-loading").addClass("loaded");
+	$(".p-line").addClass("line-start");
+	$(".p-header__ttl:nth-child(2)").addClass("js-header2");
+	$(".p-header__ttl:nth-child(3)").addClass("js-header3");
+	$(".p-header__ttl:nth-child(4)").addClass("js-header4");
+	$(".p-header__ttl:nth-child(5)").addClass("js-header5");
+	$(".p-front-page__gif").animate({ opacity: 1 }, { duration: 4000, easing: "swing" });
+	$(".p-front-page__circle").animate({ opacity: 0.3 }, { duration: 4000, easing: "swing" });
+	$(".p-nav").animate({ opacity: 1 }, { duration: 3500, easing: "swing" }); //同日2回目のアクセスでローディング画面非表示
+}
+
 jQuery(".p-nav__btn").on("click", function () {
 	event.preventDefault();
 	var link = $(this).attr("href");
@@ -69,7 +110,7 @@ window.onpageshow = function (event) {
 
 window.onload = function () {
 	var ref = document.referrer;
-	var result = ref.match(/manstandsup/);
+	var result = ref.match(/portfolio/);
 
 	if (!result) {
 		//含まれない場合の処理(ローディング画面が出る場合)
